a956965f2e51f2fd0a138304f45fcfa6350a27f4
Dmitrii Nasedkin <2_tone@mail.ru>
2021-04-22 19:49:01 +0300

    added new test for 100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,16 +18,16 @@ class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
-//    @Test
-//    void second_findMax() {
-//        StatisticsService service = new StatisticsService();
-//
-//        long[] incomesInBillions = {1, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-//        long expected = 12;
-//     long actual = service.findMax(incomesInBillions);
-//
-//        assertEquals(expected, actual);
-//    }
-//
+    @Test
+    void second_findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+     long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 
 }
